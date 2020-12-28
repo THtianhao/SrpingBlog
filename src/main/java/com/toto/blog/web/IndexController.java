8f19eccf84709539a7c00f58a1051948ae8da71e
tianhao <tianhao@cmcm.com>
2020-12-28 18:00:04 +0800

    导入静态页面以及fragment的使用

--- src/main/java/com/toto/blog/web/IndexController.java
+++ src/main/java/com/toto/blog/web/IndexController.java
@@ -3,16 +3,23 @@ package com.toto.blog.web;
 import com.toto.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
     public String index() {
-//        int i = 9/0;
+        int i = 9/0;
         String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("博客不存在");
-        }
+//        if (blog == null) {
+//            throw new NotFoundException("博客不存在");
+//        }
+
+        System.out.println("---index---");
         return "index.html";
+    }
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
     }
 }
